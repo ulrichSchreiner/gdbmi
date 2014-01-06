a456a5510db2148ff9500872a7ca3e80732f4ea7
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2014-01-06 15:41:01 +0100

    added new test-file for binary

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "World"
+	s2 := "Hello"
+	s3 := sub(s, s2)
+	fmt.Printf("%s\n", s3)
+}
+
+func sub(s1, s2 string) string {
+	return fmt.Sprintf("%s - %s", s1, s2)
+}
